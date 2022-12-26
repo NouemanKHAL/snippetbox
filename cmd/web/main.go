70841d81cf7c19dd4ce950344396ab1bf43b2357
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2022-12-27 00:40:55 +0100

    refactoring + dependency injection + centralized error management

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	config   config
+}
+
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
-	mux := http.NewServeMux()
+	// parse the flags
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static/", "Path to static assets")
+	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// prepare dependencies
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	app := application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		config:   cfg,
+	}
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	// run server
+	srv := http.Server{
+		Addr:     app.config.addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", app.config.addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
---
+++ cmd/web/route.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// serve static assets
+	fileServer := http.FileServer(http.Dir(app.config.staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// regular routes
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
